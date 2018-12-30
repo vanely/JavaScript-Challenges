ec760067ccd6dd15106e4649b7fbd0616f9c2b88
vanely <vanelys94@gmail.com>
2018-12-30 18:13:50 -0500

    JavaScript-Challenges

--- CalculatingWithFunctions/script2.js
+++ CalculatingWithFunctions/script2.js
@@ -1,3 +1,9 @@
+// There must be a function for each number from 0("zero") to 9("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy(divided_by in Ruby)
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+// Divison should be integer division, e.g eight(dividedBy(three())) / eight(divided_by(three)) should return 2, not 2.666666...
+
 function operation(a, operator, b) {
     if (operator === '*') {
         return a * b;
