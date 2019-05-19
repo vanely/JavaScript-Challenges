fca45627b845aa30f66246606fd775daaa289060
vanely <vanelys94@gmail.com>
2019-05-19 00:30:20 -0400

    javascript challenge

--- OrderedCountOfChars/script2.js
+++ OrderedCountOfChars/script2.js
@@ -31,12 +31,19 @@ let orderedCount = function (text) {
   return countTuples;
 }
 
-console.log(orderedCount(str3));
+// console.log(orderedCount(str3));
+console.log('a: ', str1.split('a').length - 1);
+console.log('b: ', str1.split('b').length - 1);
+console.log('r: ', str1.split('r').length - 1);
+console.log('c: ', str1.split('c').length - 1);
+console.log('d: ', str1.split('d').length - 1);
+
 
 // METHOD 2
-const orderedCount = s =>
+const orderedCount2 = s => // runs check for availability to increment or append 1 in set param
   Array.from(s.split('').reduce((m, k) => m.set(k, m.has(k) ? m.get(k) + 1 : 1), new Map()));
 
 
 // METHOD 3
-const orderedCount = str => [...new Set([...str])].map(char => [char, str.split(char).length - 1])
+// str.split(char).length - 1 returns the number of times said character occurs in string
+const orderedCount3 = str => [...new Set([...str])].map(char => [char, str.split(char).length - 1])
