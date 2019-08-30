23ea1d9940704c48fbe2f0b82dbdeb62f419e966
vanely <vanelys94@gmail.com>
2019-08-30 14:07:40 -0400

    Departmen number permutations complete

--- DepartmentNumbersPermutations/script.js
+++ DepartmentNumbersPermutations/script.js
@@ -8,3 +8,25 @@
  *   Fire # + Police # + Sanitation # = 12
  *   Police # must be even.
  */
+
+const depNums = () => {
+    const perms = [];
+
+    for (let i = 1; i <=7; i++) {
+        for (let j = 1; j <= 7; j++) {
+            for (let k = 1; k <= 7; k++) {
+                if ((i + j + k) === 12) {
+                    if ((i % 2 === 0) || (j % 2 === 0) || (k % 2 === 0)) {
+                        if (new Set([i, j, k]).size === 3) {
+                            perms.push([i, j, k]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return perms;
+};
+
+console.log(depNums());
