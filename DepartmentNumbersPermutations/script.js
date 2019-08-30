232b0f88efaef1dde4567100a9381dafeedbe6a1
vanely <vanelys94@gmail.com>
2019-08-30 13:27:31 -0400

    Department numbers. Permutations that equal 12

---
+++ DepartmentNumbersPermutations/script.js
@@ -0,0 +1,10 @@
+/*Departments
+ * - Fire (1 - 7)
+ * - Police (1 - 7)
+ * - Sanitation (1 - 7)
+ *
+ *   Each assigned a number in range 1 - 7
+ *   Fire #, Police #, Sanitation #. All different! 
+ *   Fire # + Police # + Sanitation # = 12
+ *   Police # must be even.
+ */
