d1d4cc434f177134cc25a63902f520cdc54d9c31
vanely <vanelys94@gmail.com>
2019-09-22 18:52:21 -0400

    problem

--- Leetcode/easy/TwoSum/script.js
+++ Leetcode/easy/TwoSum/script.js
@@ -3,20 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+
+const numArrOne = [2, 7, 11, 8, 14, 6]; // 9
+const numArrTwo = [3, 4, 2, 8, 15, 20]; // 6
+const numArrThree = [16, 22, 7, 4, 8]; // 12
+
+const twoSum = function (nums, target) {
     const output = [];
     let indexCount = 0
     let selectedNum = nums[indexCount];
 
-    nums.forEach((num, index) => {
-        if ((selectedNum + num === target) && (selectedNum !== num)) {
-            output.push(indexCount, index);
+    for(let i = 0; i < nums.length; i++) {
+        if ((selectedNum + nums[i] === target) && (selectedNum !== nums[i])) {
+            output.push(selectedNum, i);
+            break;
         }
-
-        if (output.length === 0) {
+        // can't happen in loop, incrementation will always happen
+        if ((output.length === 0) && (i === nums.length - 1)) {
+            i = 0;
             indexCount++;
-        } else {
-            return output;
         }
-    });
+    }
+    return output;
 };
+
+// function looper(start, select, nums, target) {
+//     for (let i = start; i < nums.length; i++) {
+//         if () {
+
+//         }
+//     }
+// }
+
+console.log(twoSum(numArrOne, 9));
