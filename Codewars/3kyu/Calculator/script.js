68ff153a8d2fd43081acfac8c6efacf175494403
vanely <vanelys94@gmail.com>
2021-06-24 14:26:17 -0400

    calculator challenge

---
+++ Codewars/3kyu/Calculator/script.js
@@ -0,0 +1,30 @@
+/**
+ * https://www.codewars.com/kata/5235c913397cbf2508000048/train/javascript
+ * 
+ * Create a simple calculator that given a string of operators (), +, -, *, / and numbers separated by spaces returns the value of that expression
+ * 
+ * Example:
+ * Calculator().evaluate("2 / 2 + 3 * 4 - 6") # => 7
+ */
+
+// remember PEMDAS
+const op1 = "2 / 2 + 3 * 4 - 6"
+const op2 = "127"
+const op3 = "2 - 3 - 4"
+const op4 = "10 * 5 / 2"
+
+function Calculator() {
+  this.operation = "";
+}
+
+Calculator.prototype.evaluate = function(operation) {
+  this.operation = operation;
+  return this.operation;
+}
+
+const calc = new Calculator()
+
+console.log(calc.evaluate(op1));
+console.log(calc.evaluate(op2));
+console.log(calc.evaluate(op3));
+console.log(calc.evaluate(op4));
